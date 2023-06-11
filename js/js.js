e06b9ac6fabacb57e991496a8489158100e7ab83
rickfogk <fogaca135@gmail.com>
2023-06-11 10:41:30 -0300

    Risk MAtrix adeded

--- js/js.js
+++ js/js.js
@@ -28,6 +28,7 @@ function updateLocalStorage() {
     localStorage.setItem('Total_Score', totalScore);
 }
 
+/*
 //Calculate Score Function
 function calculateScore(value, checkbox) {
   // retrieve the checkbox's id number
@@ -46,51 +47,91 @@ function calculateScore(value, checkbox) {
 
   // Show/Hide the mitigation elements based on the Total_Score and whether the checkbox is checked
   var mitigationElements = document.getElementById('mitigation' + checkboxId);
+  
   var inputs = mitigationElements.querySelectorAll('input, button');
+  
+
+    if (totalScore > 21 && checkbox.checked) {
+      mitigationElements.style.display = 'block';
+      for (var j = 0; j < inputs.length; j++) {
+        inputs[j].disabled = false;
+      }
+    } else {
+      mitigationElements.style.display = 'none';
+    }
+  }
+*/
+/*
+function calculateScore(value, checkbox) {
+  var checkboxId = checkbox.id.replace('checkbox', '');
+
+  if (checkbox.checked) {
+    totalScore += value;
+    checkedItems[checkbox.id] = value;
+    checkbox.disabled = true;  // disable the checkbox
+  } else {
+    totalScore -= checkedItems[checkbox.id];
+    delete checkedItems[checkbox.id];
+  }
+
+  document.getElementById('totalScore').innerHTML = "Total Score: " + totalScore;
+  classifyRisk(totalScore);
+
+  var mitigationElements = document.getElementById('mitigation' + checkboxId);
+  if (mitigationElements) {
+    var inputs = mitigationElements.querySelectorAll('input, button');
+
+    if (totalScore > 21 && checkbox.checked) {
+      mitigationElements.style.display = 'block';
+      for (var j = 0; j < inputs.length; j++) {
+        inputs[j].disabled = false;
+      }
+    } else {
+      mitigationElements.style.display = 'none';
+    }
+  }
+}
+*/
+//Calculate Score Function
+function calculateScore(value, checkbox) {
+  var checkboxId = checkbox.id.replace('checkbox', '');
+
+  if (checkbox.checked) {
+    totalScore += value;
+    checkedItems[checkbox.id] = value;
+    
+  } else {
+    totalScore -= checkedItems[checkbox.id];
+    //delete checkedItems[checkbox.id];
+  }
+
+  document.getElementById('totalScore').innerHTML = "Total Score: " + totalScore;
+  classifyRisk(totalScore); 
+
+  // Show/Hide the mitigation elements based on the Total_Score and whether the checkbox is checked
+  var mitigationElements = document.getElementById('mitigation' + checkboxId);
   
   if (totalScore > 21 && checkbox.checked) {
     mitigationElements.style.display = 'block';
-    for (var j = 0; j < inputs.length; j++) {
-      inputs[j].disabled = false;
-    }
+    var dropdown = mitigationElements.querySelector('select');
+    var applyButton = mitigationElements.querySelector('button');
+    if (dropdown) dropdown.disabled = false;
+    if (applyButton) applyButton.disabled = false;
   } else {
     mitigationElements.style.display = 'none';
   }
 }
 
 
-// Call classifyRisk function
-//classifyRisk(totalScore);
 
-
-
-
-
-
-/*function applyMitigation(checkboxID) {
-    let mitigationRadioYes = document.querySelector(`input[name='${checkboxID}Mitigation']:checked`);
-    let mitigationAction = document.getElementById(checkboxID + 'MitigationAction').value;
-
-    if (mitigationRadioYes && mitigationRadioYes.value === 'Yes' && mitigationAction) {
-      totalScore = totalScore - 50; // recalculate the total score by subtracting 50
-      document.getElementById('totalScore').innerText = "Total Score: " + totalScore;
-      classifyRisk(totalScore);
-
-      // disable the Apply button once the action is applied
-      document.getElementById('applyButton' + checkboxID).disabled = true;
-    } else {
-      alert("Mitigation action must be filled out and radio button must be set to 'Yes'.");
-    }
-    // Update localStorage with current scores
-    updateLocalStorage();
-}
-*/
 
 function applyMitigation(button) {
   //var checkboxId = button.getAttribute('data-checkbox-id'); 
   var checkboxId = button.getAttribute('data-checkbox-id').replace('checkbox','');
+ 
 
   var checkbox = document.getElementById('checkbox' + checkboxId);
+  
   
   // Check if checkbox exists
   if(checkbox) {
@@ -110,10 +151,13 @@ function applyMitigation(button) {
 
 
 function classifyRisk(score) {
+  var classificationElement = document.getElementById('classification');
     if (score <= 21) {
       document.getElementById('classification').innerText = "Classification: No special approval required";
-    } else if (score <= 35) {
+      classificationElement.style.color = 'blue';
+    } else if (score <= 50) {
       document.getElementById('classification').innerText = "Classification: Ops Manager Approval required";
+      classificationElement.style.color = 'red';
     } else {
       document.getElementById('classification').innerText = "Classification: Director Approval required";
     }
@@ -138,4 +182,53 @@ applyButtons.forEach(button => {
       applyMitigation(this); // pass the button element itself
   });
 
+});
+
+function resetForm() {
+  // Reset the score
+  totalScore = 0;
+  document.getElementById('totalScore').innerHTML = "Total Score: 0";
+  classifyRisk(totalScore);
+
+  // Reset the checkboxes
+  for (var key in checkedItems) {
+    var checkbox = document.getElementById(key);
+    if (checkbox) {
+      checkbox.checked = false;
+      checkbox.disabled = false;
+    }
+
+    // Hide mitigation elements
+    var mitigationElements = document.getElementById('mitigation' + key.replace('checkbox', ''));
+    if (mitigationElements) {
+      mitigationElements.style.display = 'none';
+      var inputs = mitigationElements.querySelectorAll('input, button');
+      for (var j = 0; j < inputs.length; j++) {
+        inputs[j].disabled = true;
+      }
+    }
+  }
+
+  // Clear the checked items object
+  checkedItems = {};
+}
+
+//IMSAFE Section
+// IMSAFE Section
+document.getElementById('imsafeButton').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('imsafeSection').style.display = 'block';
+});
+
+document.getElementById('calculateImsafe').addEventListener('click', function() {
+  var illness = parseInt(document.getElementById('illness').value);
+  var medication = parseInt(document.getElementById('medication').value);
+  var stress = parseInt(document.getElementById('stress').value);
+  var alcohol = parseInt(document.getElementById('alcohol').value);
+  var fatigue = parseInt(document.getElementById('fatigue').value);
+  var emotion = parseInt(document.getElementById('emotion').value);
+
+  var imsafeScore = illness + medication + stress + alcohol + fatigue + emotion;
+
+  document.getElementById('imsafeScoreDisplay').innerHTML = "IMSAFE Score: " + imsafeScore;
 });
