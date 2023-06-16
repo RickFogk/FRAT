8a728733ceea438b3d3479073fd7e2fb5ae9d6ce
rickfogk <fogaca135@gmail.com>
2023-06-16 08:45:57 -0300

    Table2 Flight ID

--- js/js.js
+++ js/js.js
@@ -1,5 +1,3 @@
-
-
 function handleMitigationAction(buttonId) {
   var button = document.getElementById(buttonId);
   var checkboxId = button.getAttribute('data-checkbox-id');
@@ -19,7 +17,6 @@ function handleMitigationAction(buttonId) {
 var totalScore = 0;
 var checkedItems = {};
 
-
 // Update the display with the initial scores
 document.getElementById('totalScore').innerHTML = "Total Score: " + totalScore;
 
@@ -28,81 +25,20 @@ function updateLocalStorage() {
     localStorage.setItem('Total_Score', totalScore);
 }
 
-/*
-//Calculate Score Function
+// Calculate Score Function
 function calculateScore(value, checkbox) {
-  // retrieve the checkbox's id number
   var checkboxId = checkbox.id.replace('checkbox', '');
 
   if (checkbox.checked) {
-    totalScore += value;
-    checkedItems[checkbox.id] = value;
-  } else {
-    totalScore -= checkedItems[checkbox.id];
-    delete checkedItems[checkbox.id];
-  }
-
-  document.getElementById('totalScore').innerHTML = "Total Score: " + totalScore;
-  classifyRisk(totalScore); // <---- ADD THIS LINE
-
-  // Show/Hide the mitigation elements based on the Total_Score and whether the checkbox is checked
-  var mitigationElements = document.getElementById('mitigation' + checkboxId);
-  
-  var inputs = mitigationElements.querySelectorAll('input, button');
-  
-
-    if (totalScore > 21 && checkbox.checked) {
-      mitigationElements.style.display = 'block';
-      for (var j = 0; j < inputs.length; j++) {
-        inputs[j].disabled = false;
-      }
-    } else {
-      mitigationElements.style.display = 'none';
+    if (!checkedItems.hasOwnProperty(checkbox.id)) {
+      totalScore += value;
+      checkedItems[checkbox.id] = value;
     }
-  }
-*/
-/*
-function calculateScore(value, checkbox) {
-  var checkboxId = checkbox.id.replace('checkbox', '');
-
-  if (checkbox.checked) {
-    totalScore += value;
-    checkedItems[checkbox.id] = value;
-    checkbox.disabled = true;  // disable the checkbox
   } else {
-    totalScore -= checkedItems[checkbox.id];
-    delete checkedItems[checkbox.id];
-  }
-
-  document.getElementById('totalScore').innerHTML = "Total Score: " + totalScore;
-  classifyRisk(totalScore);
-
-  var mitigationElements = document.getElementById('mitigation' + checkboxId);
-  if (mitigationElements) {
-    var inputs = mitigationElements.querySelectorAll('input, button');
-
-    if (totalScore > 21 && checkbox.checked) {
-      mitigationElements.style.display = 'block';
-      for (var j = 0; j < inputs.length; j++) {
-        inputs[j].disabled = false;
-      }
-    } else {
-      mitigationElements.style.display = 'none';
+    if (checkedItems.hasOwnProperty(checkbox.id)) {
+      totalScore -= checkedItems[checkbox.id];
+      delete checkedItems[checkbox.id];
     }
-  }
-}
-*/
-//Calculate Score Function
-function calculateScore(value, checkbox) {
-  var checkboxId = checkbox.id.replace('checkbox', '');
-
-  if (checkbox.checked) {
-    totalScore += value;
-    checkedItems[checkbox.id] = value;
-    
-  } else {
-    totalScore -= checkedItems[checkbox.id];
-    //delete checkedItems[checkbox.id];
   }
 
   document.getElementById('totalScore').innerHTML = "Total Score: " + totalScore;
@@ -122,9 +58,9 @@ function calculateScore(value, checkbox) {
   }
 }
 
+
 // Create an object to store checkbox IDs and scores
 let appliedMitigations = {};
-
 
 function applyMitigation(button) {
   var checkboxId = button.getAttribute('data-checkbox-id').replace('checkbox','');
@@ -136,7 +72,9 @@ function applyMitigation(button) {
     var mitigationScore = originalValue / 2;
     var oldTotalScore = totalScore; // store the original total score
     totalScore -= mitigationScore; // subtract the mitigation score from total score
-    checkbox.value = originalValue - mitigationScore; // update the checkbox value
+    var newValue = originalValue - mitigationScore;
+    checkbox.value = newValue; // update the checkbox value
+    checkedItems[checkbox.id] = newValue; // update the value in checkedItems
 
     document.getElementById('totalScore').innerHTML = "Total Score: " + totalScore;
 
@@ -148,7 +86,7 @@ function applyMitigation(button) {
       'Selected Checkbox ID': checkboxId,
       'Original Value': originalValue,
       'Mitigation Score': mitigationScore,
-      'New Checkbox Value': checkbox.value,
+      'New Checkbox Value': newValue,
       'Original Total Score': oldTotalScore,
       'New Total Score': totalScore
     };
@@ -161,36 +99,37 @@ function applyMitigation(button) {
 }
 
 
+
 function classifyRisk(score) {
   var classificationElement = document.getElementById('classification');
-    if (score <= 21) {
-      document.getElementById('classification').innerText = "Classification: No special approval required";
-      classificationElement.style.color = 'blue';
-    } else if (score <= 50) {
-      document.getElementById('classification').innerText = "Classification: Ops Manager Approval required";
-      classificationElement.style.color = 'red';
-    } else {
-      document.getElementById('classification').innerText = "Classification: Director Approval required";
-    }
-    // Update localStorage with current scores
-    updateLocalStorage();
+  if (score <= 21) {
+    document.getElementById('classification').innerText = "Classification: No special approval required";
+    classificationElement.style.color = 'blue';
+  } else if (score <= 50) {
+    document.getElementById('classification').innerText = "Classification: Ops Manager Approval required";
+    classificationElement.style.color = 'red';
+  } else {
+    document.getElementById('classification').innerText = "Classification: Director Approval required";
+  }
+  // Update localStorage with current scores
+  updateLocalStorage();
 }
 
 // Event listener for checkboxes
 var checkboxes = document.querySelectorAll('input[type=checkbox]');
 checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', function() {
-        var value = parseInt(checkbox.value);
-        calculateScore(value, checkbox);
-    });
+  checkbox.addEventListener('change', function() {
+    var value = parseInt(checkbox.value);
+    calculateScore(value, checkbox);
+  });
 });
 
 // Event listener for Apply buttons
 var applyButtons = document.querySelectorAll('.apply-button');
 applyButtons.forEach(button => {
   button.addEventListener('click', function(event) {
-      event.preventDefault(); // prevent form submission
-      applyMitigation(this); // pass the button element itself
+    event.preventDefault(); // prevent form submission
+    applyMitigation(this); // pass the button element itself
   });
 
 });
@@ -224,7 +163,6 @@ function resetForm() {
   checkedItems = {};
 }
 
-//IMSAFE Section
 // IMSAFE Section
 document.getElementById('imsafeButton').addEventListener('click', function(event) {
   event.preventDefault();
@@ -255,7 +193,10 @@ document.getElementById('resetImsafe').addEventListener('click', function() {
   document.getElementById('imsafeScoreDisplay').innerHTML = '';
 });
 
+//-----------------------------------------------------------------
 
+
+// -----------------------------------------------------------------
 // Function to create the HTML table
 function createTable() {
   // Get the table container
@@ -266,7 +207,7 @@ function createTable() {
   var thead = document.createElement('thead');
   var tbody = document.createElement('tbody');
   var headerRow = document.createElement('tr');
-  
+
   // Create table headers
   var headers = ["Checkbox Name", "Value"];
   headers.forEach(function(header) {
@@ -277,25 +218,25 @@ function createTable() {
 
   // Add the header row to the table header
   thead.appendChild(headerRow);
-  
-  // Add each checked item to the table body
-for (var checkboxId in checkedItems) {
-  var row = document.createElement('tr');
-  
-  // Get the label text of the input element
-  var checkbox = document.getElementById(checkboxId);
-  var checkboxName = checkbox.parentElement.innerText.trim();
 
-  var nameCell = document.createElement('td');
-  nameCell.appendChild(document.createTextNode(checkboxName));
-  row.appendChild(nameCell);
-  
-  var valueCell = document.createElement('td');
-  valueCell.appendChild(document.createTextNode(checkedItems[checkboxId]));
-  row.appendChild(valueCell);
-  
-  tbody.appendChild(row);
-}
+  // Add each checked item to the table body
+  for (var checkboxId in checkedItems) {
+    var row = document.createElement('tr');
+
+    // Get the label text of the input element
+    var checkbox = document.getElementById(checkboxId);
+    var checkboxName = checkbox.parentElement.innerText.trim();
+
+    var nameCell = document.createElement('td');
+    nameCell.appendChild(document.createTextNode(checkboxName));
+    row.appendChild(nameCell);
+
+    var valueCell = document.createElement('td');
+    valueCell.appendChild(document.createTextNode(checkedItems[checkboxId]));
+    row.appendChild(valueCell);
+
+    tbody.appendChild(row);
+  }
 
   // Add the header and body to the table
   table.appendChild(thead);
@@ -308,7 +249,7 @@ for (var checkboxId in checkedItems) {
 
   // Add the new table to the container
   tableContainer.appendChild(table);
-  
+
   // Add total score to the container
   var totalScoreDiv = document.createElement('div');
   totalScoreDiv.innerHTML = "Total Score: " + totalScore;
@@ -333,6 +274,70 @@ applyButtons.forEach(button => {
 document.querySelector('form').addEventListener('submit', function(event) {
   event.preventDefault();
 });
+
+// CREATE TABLE2
+// Function to create the HTML table
+function createTable2() {
+  // Get the form elements
+  var formElements = document.getElementById('flightInfoForm').elements;
+
+  // Get the table container
+  var tableContainer2 = document.getElementById('tableContainer2');
+
+  // Create table elements
+  var table = document.createElement('table');
+  var thead = document.createElement('thead');
+  var tbody = document.createElement('tbody');
+  var headerRow = document.createElement('tr');
+
+  // Create table headers
+  var headers = ["Label", "Value"];
+  headers.forEach(function(header) {
+    var th = document.createElement('th');
+    th.appendChild(document.createTextNode(header));
+    headerRow.appendChild(th);
+  });
+
+  // Add the header row to the table header
+  thead.appendChild(headerRow);
+
+  // Add each form element to the table body
+  for (var i = 0; i < formElements.length; i++) {
+    if (formElements[i].type !== "submit") { // skip the submit button
+      var row = document.createElement('tr');
+
+      var nameCell = document.createElement('td');
+      nameCell.appendChild(document.createTextNode(formElements[i].name));
+      row.appendChild(nameCell);
+
+      var valueCell = document.createElement('td');
+      valueCell.appendChild(document.createTextNode(formElements[i].value));
+      row.appendChild(valueCell);
+
+      tbody.appendChild(row);
+    }
+  }
+
+  // Add the header and body to the table
+  table.appendChild(thead);
+  table.appendChild(tbody);
+
+  // Remove any existing table from the container
+  while (tableContainer2.firstChild) {
+    tableContainer2.firstChild.remove();
+  }
+
+  // Add the new table to the container
+  tableContainer2.appendChild(table);
+}
+
+// Event listener for form submission
+document.getElementById('flightInfoForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  createTable2();
+});
+
+
 
 // Call the createTable function initially to create the table
 createTable();
